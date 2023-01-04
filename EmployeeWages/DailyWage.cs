0e0cbc232b68b48d509d4a24847e499d84b29b2c
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:41:37 +0530

    [Roshan]Added:Calculate Daily Wages Of Employee

---
+++ EmployeeWages/DailyWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class DailyWage
+    {
+        public static void CalculateDailyWage()
+        {
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);//0,1
+            Console.WriteLine("Random generated value" + empcheck);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("Employee Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+                empHrs = 0;
+
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Daily emp wage is " + empWage);
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -12,12 +12,15 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     EmployeeCheckAttendance.EmployeePresentAbsent();
+                    break;
+                case 2:
+                    DailyWage.CalculateDailyWage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
