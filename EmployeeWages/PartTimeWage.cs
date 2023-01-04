ff8f836ac4dd6d6b5e3edc4020851ecd25157e54
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:48:23 +0530

    [Roshan]Added:Calculation Of Part Time Wages Of Employee

---
+++ EmployeeWages/PartTimeWage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class PartTimeWage
+    {
+        public static void CalculatePartTimeWage()
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+
+            Random random = new Random();
+            int empCheck = random.Next(1, 3); //1,2
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is present for full time");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is part time");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp daily wage including part time also is : " + empWage);
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -12,7 +12,7 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,6 +21,9 @@ namespace EmployeeWageProgram
                     break;
                 case 2:
                     DailyWage.CalculateDailyWage();
+                    break;
+                case 3:
+                    PartTimeWage.CalculatePartTimeWage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
