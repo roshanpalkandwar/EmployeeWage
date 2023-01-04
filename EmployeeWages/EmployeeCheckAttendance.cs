afcf0e681ae8d72ecca8a87e7145310e5caa38a4
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:35:01 +0530

    [Roshan]Added:Check Employee Present Or Absent

---
+++ EmployeeWages/EmployeeCheckAttendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeCheckAttendance
+    {
+        public static void EmployeePresentAbsent()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            Console.WriteLine("Random Generated value " + empCheck);
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeWages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    EmployeeCheckAttendance.EmployeePresentAbsent();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong Choice");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
