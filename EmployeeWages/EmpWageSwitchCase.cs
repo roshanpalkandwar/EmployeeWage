0bfd1a1d0bf9a0d08804f16b744417a751e92ceb
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 19:58:46 +0530

    [Roshan]Added:Daily Employee Wages Using Switch Cases

---
+++ EmployeeWages/EmpWageSwitchCase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmpWageSwitchCase
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public static void WageForMonth()
+        {
+
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+
+                Random random = new Random();
+                int empCheck = random.Next(1, 3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Daily Emp Wage is {0} Total Emp Wage {1} ", empWage, totalEmpWage);
+            }
+            Console.WriteLine("Total Emp Wage is: " + totalEmpWage);
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -12,7 +12,7 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,9 @@ namespace EmployeeWageProgram
                     break;
                 case 3:
                     PartTimeWage.CalculatePartTimeWage();
+                    break;
+                case 4:
+                    EmpWageSwitchCase.WageForMonth();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
