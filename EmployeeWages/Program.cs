22f67b18e4582cd49ece27812b85707aaf51769d
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-11 20:46:16 +0530

    [Roshan]Added:Create CompanyEmpWage class and let

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -12,7 +12,7 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month\t 5.wages for month\t 6.employee wages for monthly \t 7.emp wages\n8.wages for compnay\n9.emwges for miltiple company ");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month\t 5.wages for month\t 6.employee wages for monthly \t 7.emp wages\n8.wages for compnay\n9.emwges for miltiple company \n10.EmpWageBuilder");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -58,6 +58,13 @@ namespace EmployeeWageProgram
                     Console.WriteLine(BMW.ToString());
                     TCS.ComputeEmployeeWage();
                     Console.WriteLine(TCS.ToString());
+                    break;
+                case 10:
+                    EmpWageBuilder empWageBuilderArray = new EmpWageBuilder();
+                    empWageBuilderArray.addCompanyEmpWage("TATA", 20, 2, 10);
+                    empWageBuilderArray.addCompanyEmpWage("BMW", 10, 4, 20);
+                    empWageBuilderArray.addCompanyEmpWage("TCS", 10, 4, 20);
+                    empWageBuilderArray.computeEmpWage();
                     break;
             }
             Console.ReadLine();
